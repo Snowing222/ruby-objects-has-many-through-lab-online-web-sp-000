50bf08af2e03f185f2dfda3705fd458f9aa9ac65
elsa chen <snowing222@gmail.com>
2020-06-02 01:57:18 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,5 +6,16 @@ class Doctor
     @@all<<name
   end
 
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor==self}
+  end
+
+  def new_appointment(date,patient)
+    Appointment.new(date,patient,self)
+  end
+
+  def patients
+    appointments.map {|appointment| appointment.patient}
+
 
 end
