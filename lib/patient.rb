a88e64a9559e558c770a1aa87906b02144007e9e
elsa chen <snowing222@gmail.com>
2020-06-02 01:54:02 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient
+  @@all
+  attr_accessor :name
+  def initialize(name)
+    @name=name
+    @@all<<self
+  end
+
+  def new_appointment
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+  end
+
+  def doctors
+  end
+end
